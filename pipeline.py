3f7203ad0a8a69223a538b9c450b570bf1ed161c
Justin Aquilante <JWaquilante@gmail.com>
2024-03-11 16:20:00 -0400

    abstracted query process to a function

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,23 @@
+from pprint import pprint
+
 import pytesseract
 from transformers import pipeline
 import torch
 from datasets import load_dataset, Image
 from tqdm.auto import tqdm
 from transformers.pipelines.base import KeyDataset
+
+def query(imageset, prompt, pipe):
+    guesses = []
+    for img in tqdm(imageset):  # Answer is on page 114
+        result = pipe(img,
+                      prompt,
+                      max_answer_len=25)
+        guesses = guesses + result
+    for i in range(len(guesses)):
+        guesses[i]['prompt'] = prompt
+    return guesses
+
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 dataset = load_dataset("imagefolder", data_dir=r'image_base')
@@ -12,10 +26,14 @@ extractive_pipe = pipeline(
     "document-question-answering",
     model="impira/layoutlm-document-qa", device='cuda'
 )
+
 answers = []
-print(dataset.values())
-for img in tqdm(dataset['train']['image'][110:120]): #Answer is on page 114
-    result = extractive_pipe(img, "What is the action / description next to the lighting message \"break lamp bulb fault?\"", max_answer_len=50)
-    answers = answers + result if result[0]['answer'] is not None else answers
+ds = dataset['train']['image'][110:120]
+
+answers += query(ds, "What causes break lamp bulb fault?", extractive_pipe)
+answers += query(ds, "Why does my screen say break lamp bulb fault?", extractive_pipe)
+answers += query(ds, "What is break lamp bulb fault?", extractive_pipe)
+
 answers = sorted(answers, key=lambda s: s['score'], reverse=True)
-print(answers)
+
+pprint(answers, sort_dicts=False)
